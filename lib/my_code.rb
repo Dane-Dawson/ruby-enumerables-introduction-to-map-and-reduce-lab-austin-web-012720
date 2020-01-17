cb30b50ed19d13e0d054c6135a4e88d60e4b2d25
Dane P Dawson <dane.p.dawson@gmail.com>
2020-01-17 03:42:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -59,9 +59,10 @@ end
 def reduce_to_all_true(source_array)
     index = 0 
     total = 0
+    empty = 0
   while index < source_array.length do
     if source_array[index] == false 
-    
+     return empty
     total += source_array[index]
     index += 1 
   end
