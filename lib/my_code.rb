5cbb3de71dc4016fe10dd0a649343e471aa57ed0
Dane P Dawson <dane.p.dawson@gmail.com>
2020-01-17 03:28:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -47,7 +47,7 @@ end
 
 def reduce_to_total(source_array, starting_point)
     index = 0 
-    total = 0
+    total = starting_point
   while index < source_array.length do
     total += source_array[index]
     index += 1 
